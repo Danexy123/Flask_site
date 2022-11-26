28fd2a7cfb758ce1246e3767c5e2d531f0b36224
Lutsa_Daniel <danch2003@yandex.ru>
2022-11-26 19:37:19 +0500

    Create fl_site.py

---
+++ fl_site.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/index")
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/about")
+def about():
+    return "<h1>О сайте</h1>"
+
+if __name__ == "__main__":
+    # чтобы видеть все ошибки
+    app.run(debug=True)
+
+
